3deca5aadc914a1a96cffdc75ef2a4db32c6bf6c
Emilien Romulus <emilien.romulus@tuta.io>
2024-10-30 20:16:06 +1100

    moved project description to README

--- index.js
+++ index.js
@@ -7,13 +7,6 @@ import path from 'path';
 const __dirname = path.resolve();
 const files = fs.readdirSync(__dirname);
 
-/*
-localhost:8080 should take users to index.html
-localhost:8080/about should take users to about.html
-localhost:8080/contact-me should take users to contact-me.html
-404.html should display any time the user tries to go to a page not listed above.
-*/
-
 // Create a local server to receive data from
 const server = http.createServer();
 
